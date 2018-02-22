1f8d99a39c81180a35addc0f44d58c0993e7161c
owen151128 <owen151128@gmail.com>
2018-02-22 15:19:04 +0900

    TestImabeViewVo 에서 Contract.Model implements

--- app/src/main/java/com/q/testdummy/vo/TestImageViewVo.java
+++ app/src/main/java/com/q/testdummy/vo/TestImageViewVo.java
@@ -1,21 +1,27 @@
 package com.q.testdummy.vo;
 
-public class TestImageViewVo {
+import com.q.testdummy.imageview.TestImageViewAdapterContract;
+
+public class TestImageViewVo implements TestImageViewAdapterContract.Model {
     private int image;
     private String text;
 
+    @Override
     public int getImage() {
         return image;
     }
 
+    @Override
     public void setImage(int image) {
         this.image = image;
     }
 
+    @Override
     public String getText() {
         return text;
     }
 
+    @Override
     public void setText(String text) {
         this.text = text;
     }
