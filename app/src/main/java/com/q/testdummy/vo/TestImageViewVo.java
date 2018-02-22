10b38130d12d50153a9e84a6532969d1a1c3ef15
owen151128 <owen151128@gmail.com>
2018-02-22 14:32:14 +0900

    TestImageViewVo 추가
    RecyclerView 에 사용될 VauleObject Class
    맴버변수로 Image 에 대한 drawable 상수 값과
    text에 대한 String 값을 가지고 있음

---
+++ app/src/main/java/com/q/testdummy/vo/TestImageViewVo.java
@@ -0,0 +1,22 @@
+package com.q.testdummy.vo;
+
+public class TestImageViewVo {
+    private int image;
+    private String text;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
